8e4369898fb68eb95cd493175d4d003c14d08090
PC_MARCIO\marci <marcio.nielsen@gmail.com>
2024-03-31 18:23:54 -0300

    Declarando CONSTANTES.

--- my_hello/src/main.rs
+++ my_hello/src/main.rs
@@ -1,9 +1,17 @@
+
+const SECOND_IN_MINUTES : u32 = 60;
+const MINUTES_IN_HOUR : u32 = 60;
+const SECONDS_IN_HOUR : u32 = SECOND_IN_MINUTES * MINUTES_IN_HOUR;
+
 fn main() {
     // println!("Olá, mundo!");  -- primeiro programa em Rust
 
     // Variaveis e Constantes em Rust
 
-    let trabalhou : i32 = 30;
-
+    let trabalhou : u32 = 30;
     println!("Trabalhou {} horas", trabalhou);
+
+    let total_em_segundos : u32 = trabalhou * SECONDS_IN_HOUR;
+    println!("Trabalhou {} segundos", total_em_segundos);
+
 }
