f6db9aea135404bda2feec249738343f0415539d
PC_MARCIO\marci <marcio.nielsen@gmail.com>
2024-04-01 01:20:24 -0300

    Gerenciamento de memória em Rust.

--- my_hello/src/main.rs
+++ my_hello/src/main.rs
@@ -11,6 +11,40 @@ fn main() {
 
     // Tipos Primitivos 
     tipos_primiticos();
+
+    // Gerenciamento de memória em Rust
+
+    // +--------------------++-----------------------------------------------------+-------------------+----------------------+-------------------------------+
+    // | MEMORY             || CONTENTS                                            | SIZE              | LIFETIME             | CLEANUP                       |
+    // +--------------------++-----------------------------------------------------+-------------------+----------------------+-------------------------------+
+    // | |----------------| ||-------|---------------------------------------------|-------------------|----------------------|-------------------------------|      
+    // | |STATIC          | ||Static | - Binário do programa                       | Fixo              | Programa completo    | Quando o programa terminar    |
+    // | |                | ||       | - Variáveis Static                          |                   |                      |                               |    
+    // | |----------------| ||       | - Literais String                           |                   |                      |                               |    
+    // | |----------------| ||       | * saber o tamanho na compilação             |                   |                      |                               |    
+    // | | STACK          | ||-------|---------------------------------------------|-------------------|----------------------|-------------------------------|    
+    // | |-------  -------| ||Stack  | - Argumentos de funções                     | Dinâmico          | Funções              | Quando as funções terminarem  |
+    // | |       \/       | ||       | - Variáveis Locais                          | * limite superior |                      |                               |    
+    // | |----------------| ||       | - Cada thread tem uma pilha                 |                   |                      |                               |    
+    // | |                | ||       |   isolada                                   |                   |                      |                               |    
+    // | |                | ||       | - Para cada função é criada um stack frame, |                   |                      |                               |    
+    // | |  FREE          | ||       |   quando a função termina, o stack frame é  |                   |                      |                               |    
+    // | |                | ||       |   desalocado, assim como todas as variáveis |                   |                      |                               |    
+    // | |       /\       | ||       |   declaradas nele                           |                   |                      |                               |    
+    // | |-------  -------| ||       | * saber o tamanho na compilação             |                   |                      |                               |    
+    // | | HEAP           | ||-------|---------------------------------------------|-------------------|----------------------|-------------------------------|    
+    // | |                | ||Heap   | - Valores vivem além das funções            | Dinâmico          | Definido pelo        | Manualmente                   |    
+    // | |                | ||       | - Compartilhada através das threads         | * acima do limite | programador ou pela  | ou via GC                     |    
+    // | |                | ||       | - Valores grandes                           |   do computador   | linguagem            | ou via RAII                   |    
+    // | |                | ||       | - Valores de tamanhos dinâmicos             |                   |                      |                               |    
+    // | |                | ||       | * Não sabe o tamanho na compilação          |                   |                      |                               |    
+    // | |                | ||       |                                             |                   |                      |                               |   
+    // | |----------------| ||-------|---------------------------------------------|-------------------|----------------------|-------------------------------|      
+    // +--------------------++-----------------------------------------------------+-------------------+----------------------+-------------------------------+
+    
+
+
+
 }
 
 fn variaveis_constantes() {
